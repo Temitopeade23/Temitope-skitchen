c4f44d6e07d3aadde283255796598f1d984db570
Temitope <temitopeadeniyi2015@gmail.com>
2025-01-10 13:35:27 +0100

    Initial commit - Temitope's Kitchen

---
+++ src/main/java/com/temitopekitchen/TemitopeKitchenApplication.java
@@ -0,0 +1,11 @@
+package com.temitopekitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemitopeKitchenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TemitopeKitchenApplication.class, args);
+    }
+}
