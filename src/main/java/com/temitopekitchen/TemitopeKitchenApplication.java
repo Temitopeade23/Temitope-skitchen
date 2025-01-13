2e89c08b362fd26629c64fe61e3f662bf47e5c5f
Temitopeade23 <139174921+Temitopeade23@users.noreply.github.com>
2025-01-13 14:51:07 +0100

    Update TemitopeKitchenApplication.java

--- src/main/java/com/temitopekitchen/TemitopeKitchenApplication.java
+++ src/main/java/com/temitopekitchen/TemitopeKitchenApplication.java
@@ -2,9 +2,10 @@ package com.temitopekitchen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TemitopeKitchenApplication {
+public class TemitopeKitchenApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(TemitopeKitchenApplication.class, args);
     }
